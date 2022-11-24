37f5ce149ec0be75d4dcd863e0c133f0f0e98565
n0sman <noha.osm4n@gmail.com>
2022-11-24 15:18:50 -0500

    while

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,28 @@
 
 // first project
 
-console.log("noha's the best")
+// numbers
+// strings (text) ""  ''  ``
+// arrays
+// objects
+// functions
+
+const thebest = "noha";
+// console.log (thebest) 
+// console.log (thebest)
+
+let number = 0;
+while (number < 3) {
+    console.log(number, thebest)
+    number++; // number = number + 1
+}
+
+// number: 3
+// output:
+// 0 noha
+// 1 noha
+// 2 noha
+
+// console.log("noha's the best")
+
+// const, let
